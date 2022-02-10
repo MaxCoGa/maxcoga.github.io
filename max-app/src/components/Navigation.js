489962eaeb31e7a16aca581aa43092ca354075f4
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2022-02-09 22:37:16 -0500

    Fix mobile navigation

--- max-app/src/components/Navigation.js
+++ max-app/src/components/Navigation.js
@@ -1,48 +1,38 @@
-import React from "react";
+import React, {useRef} from "react";
 import { NavLink } from "react-router-dom";
+import {Navbar,NavDropdown,Nav,Container} from 'react-bootstrap'
 
-function Navigation() {
+const Navigation  = () => {
   return (
-    <div className="navigation">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            Max App
-          </NavLink>
-          <div>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contact
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/projects">
-                  Projects
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/blog">
-                  Blog
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      {/* <Container> */}
+      <Navbar.Brand as={NavLink} to="/">MaxApp</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
+          <Nav.Link as={NavLink} to="/blog">Blog</Nav.Link>
+          {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown> */}
+        </Nav>
+        {/* <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav> */}
+      </Navbar.Collapse>
+      {/* </Container> */}
+    </Navbar>
   );
-}
+};
 
 export default Navigation;
