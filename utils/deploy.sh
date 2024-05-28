0f1ea2586f0a362e63bbfc6f854823b9915d1f65
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2024-05-28 17:07:31 -0400

    add cicd

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,1 +1,11 @@
-mkdir src
+# npm run build
+# run from root
+mkdir ../public
+cp src/* ../public/
+cd ..
+git switch --orphan build
+git add ./public/*
+git commit -m "Build"
+git push -u origin build
+git checkout dev
+git branch build --delete
