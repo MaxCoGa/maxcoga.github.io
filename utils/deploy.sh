5e5a860cdbb1ca01e85ee215241368beab7e4435
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2024-05-28 17:33:19 -0400

    clean up

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,13 +1,10 @@
-# npm run build
-# run from root
 mkdir ../public
 cp -r ../src/* ../public/
-# cd ..
 git checkout --orphan build
 git reset
 git add ../public/*
 git commit -m "Build"
 git push -u origin build
 git checkout dev --force
-git branch build --delete
+git branch -D build
 rm ../public
