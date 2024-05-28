53358e42ed1680141a3ad1d2d1bf544bab18ce49
Max <26971160+MaxCoGa@users.noreply.github.com>
2024-05-28 17:31:33 -0400

    add workflow

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,1 +1,13 @@
-mkdir src
+# npm run build
+# run from root
+mkdir ../public
+cp -r ../src/* ../public/
+# cd ..
+git checkout --orphan build
+git reset
+git add ../public/*
+git commit -m "Build"
+git push -u origin build
+git checkout dev --force
+git branch build --delete
+rm ../public
