fe64d3ff8ed8fc06f4cff50cde6cbc7a1428cee4
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2024-05-28 17:21:54 -0400

    test

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,11 +1,12 @@
 # npm run build
 # run from root
 mkdir ../public
-cp src/* ../public/
+cp ../src/* ../public/
 # cd ..
-git switch --orphan build
-git add ./public/*
+git checkout --orphan build
+git reset
+git add ../public/*
 git commit -m "Build"
 git push -u origin build
-git checkout dev
+git checkout dev --force
 git branch build --delete
