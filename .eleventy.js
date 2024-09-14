014c90a05fd2b1343b278970a91f95b3414eb6c4
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2024-09-14 15:30:34 -0400

    responsive nav menu

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(pluginBundle);
 
     eleventyConfig.addPassthroughCopy("./src/style.css");
+	eleventyConfig.addPassthroughCopy("./src/index.js");
 	eleventyConfig.addPassthroughCopy("./src/robots.txt");
 	eleventyConfig.addPassthroughCopy("./src/post_styles.css");
 	eleventyConfig.addPassthroughCopy("./src/favicon.ico");
---
+++ src/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navOverlay").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("navOverlay").style.display = "none";
+}
