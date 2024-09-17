284b76c73ea1e27416f34e6079abc00d82363a1f
Maxime <26971160+MaxCoGa@users.noreply.github.com>
2024-09-17 16:58:08 -0400

    ci test

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/robots.txt");
 	eleventyConfig.addPassthroughCopy("./src/post_styles.css");
 	eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+	eleventyConfig.addPassthroughCopy("./src/static/assets/mks.jpg");
 
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
